1136cf8f5f294e929dd326b8016894e8caaa239b
HarishankarAyandev <harishankards@gmail.com>
2014-10-12 18:28:57 +0530

    ruby dependencies added

--- script.sh
+++ script.sh
@@ -15,6 +15,9 @@ sudo dpkg -i sublime-text_build-3047_*.deb
 #for git
 sudo apt-get install git
 
+#ruby dependencies
+sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties
+
 #for rbenv
 sudo apt-get install -y libssl-dev zlib1g-dev libreadline-dev
 git clone git://github.com/sstephenson/rbenv.git ~/.rbenv

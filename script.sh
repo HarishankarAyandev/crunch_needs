a1da1e25b9b6d7b3c6b8f5b6783c9cc84d44134f
HarishankarAyandev <harishankards@gmail.com>
2014-10-12 18:35:42 +0530

    reference link added

--- script.sh
+++ script.sh
@@ -15,6 +15,8 @@ sudo dpkg -i sublime-text_build-3047_*.deb
 #for git
 sudo apt-get install git
 
+https://gorails.com/setup/ubuntu/14.04
+
 #ruby dependencies
 sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties
 

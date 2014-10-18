5ef5241d902af2fec20c5aebd1ce247cfe2807de
HarishankarAyandev <harishankards@gmail.com>
2014-10-18 09:26:27 +0530

    zshell

--- lubuntu_things.sh
+++ lubuntu_things.sh
@@ -1,1 +1,16 @@
 http://www.everydaylinuxuser.com/2014/06/5-things-to-do-after-installing-lubuntu.html
+
+#Installing zshell
+sudo apt-get install zsh
+
+Now to set z-shell as the default shell
+ chsh -s `which zsh`
+or
+chsh -s $(which zsh)
+
+Close and Open your terminal, check the terminal, you will have new, nice prompt! ;)
+which firefox
+
+This command shows location of the executable of firefox(which is an application). Similary you could use this which command to see the location of any utility. And $() or the backticks get the value from the which command.
+
+
